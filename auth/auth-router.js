2d5c2c0da0a8e4690120eb5b8e828fae5a9b3dcb
william-parker <edwinious@gmail.com>
2019-08-25 11:43:07 -0400

    Added REGISTER code - WIP

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,36 @@
-const router = require('express').Router();
+// const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+
+const secrets = require("../config/secrets.js");
+const Users = require("../users/users-model.js");
+
+// router.post('/register', (req, res) => {
   // implement registration
+
+router.post("/register", (req, res) => {
+  if (req.body.username && req.body.password) {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 12); // hash the password
+    user.password = hash; // reset password as hashed password
+
+    Users.addUser(user)
+      .then(newUser => {
+        res.status(201).json(newUser);
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "There was an error while trying to add that user."
+        });
+      });
+  } else {
+    res.status(400).json({ message: "Please enter a username and password." });
+  }
 });
+
+
 
 router.post('/login', (req, res) => {
   // implement login
