78ed74b68f637d8a11103c069f2661f54e52f3b9
william-parker <edwinious@gmail.com>
2019-08-25 12:27:30 -0400

    Added login code, secrets file, jokes-queries file - WIP

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,15 +5,16 @@ const jwt = require("jsonwebtoken");
 
 
 const secrets = require("../config/secrets.js");
-const Users = require("../users/users-model.js");
+const Users = require("../queries/jokes-queries.js");
 
 // router.post('/register', (req, res) => {
   // implement registration
 
+  // REGISTER
 router.post("/register", (req, res) => {
   if (req.body.username && req.body.password) {
     let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 12); // hash the password
+    const hash = bcrypt.hashSync(user.password, 14); // hash the password
     user.password = hash; // reset password as hashed password
 
     Users.addUser(user)
@@ -22,7 +23,7 @@ router.post("/register", (req, res) => {
       })
       .catch(err => {
         res.status(500).json({
-          message: "There was an error while trying to add that user."
+          message: "There was an error while trying to add user."
         });
       });
   } else {
@@ -32,8 +33,41 @@ router.post("/register", (req, res) => {
 
 
 
-router.post('/login', (req, res) => {
+// router.post('/login', (req, res) => {
   // implement login
+
+// LOGIN
+  router.post("/login", (req, res) => {
+    if (req.body.username && req.body.password) {
+      let { username, password } = req.body;
+      Users.findBy({ username })
+        .first()
+        .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+            const token = createToken(user);
+            res.status(200).json({ message: `Welcome, ${user.username}`, token });
+          } else {
+            res.status(401).json({ message: "Invalid Credentials." });
+          }
+        });
+    } else {
+      res.status(400).json({ message: "Please enter a username and password." });
+    }
 });
+
+
+// CREATE TOKEN FUNCTION
+function createToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "8h"
+  };
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+
 
 module.exports = router;
